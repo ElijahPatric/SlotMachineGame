f0a524784b9b3c9f80dd6957e4ff2b7416dd8762
Elijah Patric <foreverelijah@gmail.com>
2022-11-30 16:29:30 -0600

    Initial Commit

---
+++ SlotMachineGame/SlotMachineGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineGameApp.swift
+//  SlotMachineGame
+//
+//  Created by Elijah on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
